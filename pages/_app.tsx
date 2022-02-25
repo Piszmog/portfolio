f07df876139a940a46ca40663de8c7b478d3cbde
Piszmog <piszmogcode@gmail.com>
2022-02-25 10:32:57 -0700

    add mantine and layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,37 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { ColorScheme, ColorSchemeProvider, MantineProvider } from '@mantine/core';
+import { useState } from 'react';
+import { useColorScheme } from '@mantine/hooks';
+import Main from '../components/layout/Main';
+import AppHeader from '../components/layout/AppHeader';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  const preferredColorScheme = useColorScheme();
+  const [colorScheme, setColorScheme] = useState<ColorScheme>(preferredColorScheme);
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
 
-export default MyApp
+  return (
+    <>
+      <Head>
+        <title>Portfolio</title>
+        <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width' />
+      </Head>
+
+      <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{ colorScheme }}
+        >
+          <Main header={<AppHeader title='Piszmog' />}>
+            <Component {...pageProps} />
+          </Main>
+        </MantineProvider>
+      </ColorSchemeProvider>
+    </>
+  );
+};
+
+export default App;
