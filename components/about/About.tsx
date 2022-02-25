79e53766ec87efd19aff0bfce8b29fde637c5fc4
Piszmog <piszmogcode@gmail.com>
2022-02-25 13:39:49 -0700

    update sample about component

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -1,6 +1,37 @@
+import { Box, Center, Image, Spoiler, Text } from '@mantine/core';
+
+type AboutProps = {
+  img: string;
+  description: string;
+};
+
 const About = () => {
   return (
-    <div />
+    <div style={{ maxWidth: 1000, margin: 'auto' }}>
+      <Center>
+        <Image
+          width={100}
+          height={120}
+          mb='md'
+          radius='md'
+          src='jerry.jpg'
+          alt='Profile Image'
+        />
+      </Center>
+      <Spoiler maxHeight={30} showLabel='Show more' hideLabel='Hide'>
+        <div>
+          <Text>
+            We Butter the Bread with Butter was founded in 2007 by Marcel Neumann, who was originally
+            guitarist for Martin Kesici&apos;s band, and Tobias Schultka. The band was originally meant as
+            a joke, but progressed into being a more serious musical duo. The name for the band has no
+            particular meaning, although its origins were suggested from when the two original members
+            were driving in a car operated by Marcel Neumann and an accident almost occurred. Neumann
+            found Schultka &quot;so funny that he briefly lost control of the vehicle.&quot; Many of their
+            songs from this point were covers of German folk tales and nursery rhymes.
+          </Text>
+        </div>
+      </Spoiler>
+    </div>
   );
 };
 
