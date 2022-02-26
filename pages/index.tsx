c6a20957cbf7bdd3e491e8ed96d8927d6ec79735
Piszmog <piszmogcode@gmail.com>
2022-02-25 20:29:41 -0700

    use env variables to decide data to query

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,30 +6,46 @@ import History from '../components/history/History';
 import Toolbox from '../components/toolbox/Toolbox';
 import About from '../components/about/About';
 import HelloCode from '../components/helloCode/HelloCode';
-import { FaClock, FaCode } from 'react-icons/fa';
+import { FaClock } from 'react-icons/fa';
 import { Center, Divider } from '@mantine/core';
 import jerry from '../public/jerry.jpg';
+import { getUserData } from '../lib/db';
+import getIcon from '../lib/icon';
+import AppHeader from '../components/layout/AppHeader';
+import Main from '../components/layout/Main';
 
 export const getStaticProps: GetStaticProps = async () => {
-  const user = await getUser('Piszmog');
+  const user = await getUser(process.env.GITHUB_USERNAME!);
+  const data = await getUserData(process.env.USER_NAME!);
   return {
     props: {
       user,
+      data,
     },
     revalidate: 7200,
   };
 };
 
-const Home: NextPage = ({ user }: InferGetStaticPropsType<typeof getStaticProps>) => {
+const Home: NextPage = ({ user, data }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const history = [...data.education, ...data.work];
+
   return (
-    <div>
+    <Main header={<AppHeader title={data.name} username={data.githubUsername} />}>
       <HelloCode style={{ height: 300 }} />
       <Divider mt='xl' mb='xl' />
       <Center>
         <h1>About</h1>
       </Center>
       <Center>
-        <About src={jerry} description='I am'/>
+        <About
+          src={jerry}
+          title={{ icon: getIcon(data.about.title.icon), value: data.about.title.value }}
+          subtitles={data.about.subtitles.map((subtitle: any) => ({
+            icon: getIcon(subtitle.icon),
+            value: subtitle.value,
+          }))}
+          description={data.about.description}
+        />
       </Center>
       <Divider mt='xl' mb='xl' />
       <Center>
@@ -46,42 +62,39 @@ const Home: NextPage = ({ user }: InferGetStaticPropsType<typeof getStaticProps>
         <h1>History</h1>
       </Center>
       <Center>
-        <History active={1} items={[
-          {
-            icon: <FaClock />,
-            title: 'Joined GitHub',
-            description: 'Joined GitHub on May 1, 2020',
-            start: 'Dec 2018',
-            end: 'May 2020',
-          },
-          {
-            icon: <FaClock />,
-            title: 'Joined GitHub',
-            description: 'Joined GitHub on May 1, 2020',
-            start: 'Dec 2018',
-            end: 'May 2020',
-          },
-        ]} />
+        <History
+          active={1}
+          // items={}
+          items={[
+            {
+              icon: <FaClock />,
+              title: 'Joined GitHub',
+              description: 'Joined GitHub on May 1, 2020',
+              start: 'Dec 2018',
+              end: 'May 2020',
+            },
+            {
+              icon: <FaClock />,
+              title: 'Joined GitHub',
+              description: 'Joined GitHub on May 1, 2020',
+              start: 'Dec 2018',
+              end: 'May 2020',
+            },
+          ]}
+        />
       </Center>
       <Divider mt='xl' mb='xl' />
       <Center>
         <h1>Toolbox</h1>
       </Center>
       <Toolbox
-        items={[
-          {
-            title: 'Frontend',
-            icon: <FaCode />,
-            items: ['React', 'Angular', 'Svelte'],
-          },
-          {
-            title: 'Backend',
-            icon: <FaCode />,
-            items: ['Java', 'Go', 'Rust'],
-          },
-        ]}
+        items={data.toolbox.sort((a: any, b: any) => a.type >= b.type).map((tool: any) => ({
+          icon: getIcon(tool.type),
+          title: tool.type.toUpperCase(),
+          items: tool.values,
+        }))}
       />
-    </div>
+    </Main>
   );
 };
 
