42b3e9b351b2e34383b431f74e75ceb7bf4772c0
Piszmog <piszmogcode@gmail.com>
2022-02-25 20:33:43 -0700

    change from grid to group

--- components/toolbox/Toolbox.tsx
+++ components/toolbox/Toolbox.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid } from '@mantine/core';
+import { Group } from '@mantine/core';
 import ToolboxItem from './ToolboxItem';
 
 type ToolboxProps = {
@@ -12,15 +12,13 @@ type ToolboxProps = {
 
 const Toolbox = ({ items }: ToolboxProps) => {
   return (
-    <Grid gutter='md' mt='sm'>
+    <Group position='apart' mt='sm'>
       {
         items.map((item, index) =>
-          <Grid.Col md={6} lg={4} key={index}>
-            <ToolboxItem {...item} />
-          </Grid.Col>,
+          <ToolboxItem key={index} {...item} />,
         )
       }
-    </Grid>
+    </Group>
   );
 };
 
